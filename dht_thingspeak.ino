bfb4b4306d8851a299ffc6a78bdec72f394acb71
Nat Weerawan <nat.wrw@gmail.com>
2015-06-14 12:48:03 +0700

    Reduce global variable.

--- dht_thingspeak.ino
+++ dht_thingspeak.ino
@@ -3,30 +3,6 @@
 
 #include "DHT.h"
 
-#define DHTPIN 2     // what pin we're connected to
-
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11 
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-
-// Initialize DHT sensor for normal 16mhz Arduino
-DHT dht(DHTPIN, DHTTYPE, 30);
-// NOTE: For working with a faster chip, like an Arduino Due or Teensy, you
-// might need to increase the threshold for cycle counts considered a 1 or 0.
-// You can do this by passing a 3rd parameter for this threshold.  It's a bit
-// of fiddling to find the right value, but in general the faster the CPU the
-// higher the value.  The default for a 16mhz AVR is a value of 6.  For an
-// Arduino Due that runs at 84mhz a value of 30 works.
-// Example to initialize DHT sensor for Arduino Due:
-//DHT dht(DHTPIN, DHTTYPE, 30);
 
 #include <ESP8266WiFi.h>
 
@@ -36,6 +12,8 @@ const char* password = "activegateway";
 
 const char* host = "api.thingspeak.com";
 const char* apiKey = "14UX64T1VR0YG0CE";
+
+DHT *dht;
 
 #include <OneWire.h>
 
@@ -65,19 +43,45 @@ void connectWifi() {
   Serial.println(WiFi.localIP());
 }
 
-void initDht() {
+void initDht(DHT *dht) {
+  #define DHTPIN 2     // what pin we're connected to
+
+  // Uncomment whatever type you're using!
+  //#define DHTTYPE DHT11   // DHT 11 
+  #define DHTTYPE DHT22   // DHT 22  (AM2302)
+  //#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+  // Connect pin 1 (on the left) of the sensor to +5V
+  // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+  // to 3.3V instead of 5V!
+  // Connect pin 2 of the sensor to whatever your DHTPIN is
+  // Connect pin 4 (on the right) of the sensor to GROUND
+  // Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+  // Initialize DHT sensor for normal 16mhz Arduino
+  // NOTE: For working with a faster chip, like an Arduino Due or Teensy, you
+  // might need to increase the threshold for cycle counts considered a 1 or 0.
+  // You can do this by passing a 3rd parameter for this threshold.  It's a bit
+  // of fiddling to find the right value, but in general the faster the CPU the
+  // higher the value.  The default for a 16mhz AVR is a value of 6.  For an
+  // Arduino Due that runs at 84mhz a value of 30 works.
+  // Example to initialize DHT sensor for Arduino Due:
+  //DHT dht(DHTPIN, DHTTYPE, 30); 
+
+  dht = new DHT(DHTPIN, DHTTYPE, 30); 
+  dht->begin();  
   Serial.println("DHTxx test!");
-  dht.begin();  
 }
 
 void setup() {
+
   Serial.begin(115200); 
   delay(10);
   
   setUpWifi();
   connectWifi();
-  initDht();
 
+  initDht(dht);
 }
 
 void uploadFn(float t, float h) {
@@ -252,8 +256,7 @@ void loop() {
   static float t;
   static float h;
   
-//  readDht(&dht, &t, &h);
-
+  //readDht(&dht, &t, &h);
   readDs18B20(&t);
   uploadFn(t, t);
   
