a8789f2599a89cff2d1bf88d4be106102c51841f
Nat Weerawan <nat.wrw@gmail.com>
2015-06-14 12:10:18 +0700

    Add onewire 18x20 support.

--- dht_thingspeak.ino
+++ dht_thingspeak.ino
@@ -35,12 +35,14 @@ const char* password = "activegateway";
 
 
 const char* host = "api.thingspeak.com";
-const char* apiKey = "N1GS1H8O5MT8E7";
+const char* apiKey = "14UX64T1VR0YG0CE";
 
-void setup() {
-  Serial.begin(115200); 
-  delay(10);
+#include <OneWire.h>
 
+OneWire ds(2);  // on pin 2 (a 4.7K resistor is necessary)
+
+
+void setUpWifi() {
   // We start by connecting to a WiFi network
 
   Serial.println();
@@ -48,8 +50,10 @@ void setup() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
   
-  WiFi.begin(ssid, password);
-  
+  WiFi.begin(ssid, password);  
+}
+
+void connectWifi() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -59,10 +63,21 @@ void setup() {
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  
+}
+
+void initDht() {
   Serial.println("DHTxx test!");
- 
-  dht.begin();
+  dht.begin();  
+}
+
+void setup() {
+  Serial.begin(115200); 
+  delay(10);
+  
+  setUpWifi();
+  connectWifi();
+  initDht();
+
 }
 
 void uploadFn(float t, float h) {
@@ -93,19 +108,14 @@ void uploadFn(float t, float h) {
                "Connection: close\r\n\r\n");  
 }
 
-void loop() {
-  // Wait a few seconds between measurements.
-  delay(2000);
-
-  
-
+void readDht(DHT *dht, float *temp, float *humid) {
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
+  float h = dht->readHumidity();
   // Read temperature as Celsius
-  float t = dht.readTemperature();
+  float t = dht->readTemperature();
   // Read temperature as Fahrenheit
-  float f = dht.readTemperature(true);
+  float f = dht->readTemperature(true);
   
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t) || isnan(f)) {
@@ -115,7 +125,7 @@ void loop() {
 
   // Compute heat index
   // Must send in temp in Fahrenheit!
-  float hi = dht.computeHeatIndex(f, h);
+  float hi = dht->computeHeatIndex(f, h);
   
   Serial.print("Humidity: "); 
   Serial.print(h);
@@ -129,8 +139,125 @@ void loop() {
   Serial.print(hi);
   Serial.println(" *F");
   
+  *temp = t;
+  *humid = f;
   
-  uploadFn(t, h);
+}
+
+void readDs18B20(float *temp) {
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius, fahrenheit;
+  
+  if ( !ds.search(addr)) {
+    Serial.println("No more addresses.");
+    Serial.println();
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+  
+  Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return;
+  }
+  Serial.println();
+ 
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      Serial.println("  Chip = DS18S20");  // or old DS1820
+      type_s = 1;
+      break;
+    case 0x28:
+      Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      Serial.println("Device is not a DS18x20 family device.");
+      return;
+  } 
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+  
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+  
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+
+  Serial.print("  Data = ");
+  Serial.print(present, HEX);
+  Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print(" CRC=");
+  Serial.print(OneWire::crc8(data, 8), HEX);
+  Serial.println();
+
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+  Serial.print("  Temperature = ");
+  Serial.print(celsius);
+  Serial.print(" Celsius, ");
+  Serial.print(fahrenheit);
+  Serial.println(" Fahrenheit");
+    
+    
+  *temp = celsius;
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  static float t;
+  static float h;
+  
+//  readDht(&dht, &t, &h);
+
+  readDs18B20(&t);
+  uploadFn(t, t);
+  
+  Serial.println(t);
 
   delay(15 * 1000);  
   
